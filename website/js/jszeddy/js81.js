692c4f640e853a9c82de945dc64ae4b98eef0aa7
de-mux <de-mux@users.noreply.github.com>
2025-09-06 18:53:00 -0700

    Added debugging logs

--- website/js/jszeddy/js81.js
+++ website/js/jszeddy/js81.js
@@ -7,7 +7,12 @@ function go(pfile) {
   if (req.overrideMimeType)
     req.overrideMimeType("text/plain;charset=x-user-defined");
   req.send(null);
-  roms["zx81.rom"] = req.responseText;
+  if (req.responseText) {
+    console.log("ROM loaded, size: " + req.responseText.length);
+    roms["zx81.rom"] = req.responseText;
+  } else {
+    console.warn("Warning: ROM file is empty or failed to load.");
+  }
   z80_init();
   zx81_init();
   // fskip=1; // possible override
